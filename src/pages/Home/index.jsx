60805e5de340ed401600ac6240d78aba67897879
abnerfirmino <abnercasteliano@outlook.com>
2023-03-06 21:42:57 -0300

    primeiro commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,22 @@
+import './styles.css';
+
+const Home = () => {
+  
+  return (
+    <>
+      <header>
+
+      </header>
+
+      <main>
+
+      </main>
+
+      <footer>
+        
+      </footer>
+    </>
+  );
+}
+
+export { Home };
