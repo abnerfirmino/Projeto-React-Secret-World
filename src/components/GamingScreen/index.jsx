a43aa9800b3f36f846e3203dbfa6b23ab80cf3c6
abnerfirmino <abnercasteliano@outlook.com>
2023-03-09 12:40:53 -0300

    Criando a lógica do jogo pt.2

--- src/components/GamingScreen/index.jsx
+++ src/components/GamingScreen/index.jsx
@@ -4,7 +4,29 @@ const GamingScreen = ({ verifyLetter }) => {
 
   return (
     <div className='gaming'>
-      <p>I'm playing</p>
+      <p className='points'>
+        <span>Pontuação: 000</span>
+      </p>
+      <h2>Adivinhe a palavra:</h2>
+      <h3 className='tip'>
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Inserir!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a, </span>
+        <span>b, </span>
+      </div>
       <button onClick={verifyLetter}>Finalizar o jogo</button>
     </div>
   );
