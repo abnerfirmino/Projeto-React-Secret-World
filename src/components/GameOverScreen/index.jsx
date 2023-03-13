ea0cde55ee0debdb11117636598441e5914e8039
abnerfirmino <abnercasteliano@outlook.com>
2023-03-13 20:42:23 -0300

    tela de game over finalizada

--- src/components/GameOverScreen/index.jsx
+++ src/components/GameOverScreen/index.jsx
@@ -6,7 +6,7 @@ const GameOverScreen = ({ restart, score, hits }) => {
     <div className='end'>
       <h2>Fim de Jogo</h2>
       <h3>Sua pontuação foi: <span className='score'>{score}</span></h3>
-      <p>Você acertou <span className='guesses'>{hits}</span> palavras.</p>
+      <p>Você acertou <span className='guesses'>{hits}</span> palavra(as)</p>
       <button onClick={restart}>Reiniciar o jogo</button>
     </div>
   );
